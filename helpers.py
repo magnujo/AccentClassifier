e4da413221689db9eb36b10301243f203b66b39e
Magnus <magnus_johannsen@hotmail.com>
2021-05-18 17:44:56 +0200

    Added file audiofunctions.py with functions to draw histograms of amplitude distributions

--- helpers.py
+++ helpers.py
@@ -7,14 +7,16 @@ from sklearn.preprocessing import LabelBinarizer
 def get_label_from_frame(df: DataFrame, column: str):
     distinct = df[column].unique()
 
+
 def clean_binarize(dataframe, focus_col, values_to_remove=["other"]):
     df = dataframe[["path", focus_col]]
     df = df.dropna()
-    for val in values_to_remove: #ved ikke om der er en smartere løsning
+    for val in values_to_remove:  # ved ikke om der er en smartere løsning
         df = df[df[focus_col] != val]
     lb = LabelBinarizer()
     label_binarizer = lb.fit_transform(df[focus_col])
     return df, label_binarizer
+
 
 def get_info(dataframe, focus_col):
     df = dataframe[["path", focus_col]]
